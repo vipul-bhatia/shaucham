dea58bbaa89f4d3900ab0af05cef9e1cf8dc951a
Vipul Bhatia <97945914+vipul-bhatia@users.noreply.github.com>
2024-07-01 15:28:06 +0530

    .

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const nodemailer = require('nodemailer');
 
-let serviceAccount = require("./shaucham-web-firebase-adminsdk-g8lbd-8663865fcf.json");
+// let serviceAccount = require("./shaucham-web-firebase-adminsdk-g8lbd-8663865fcf.json");
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// const config = {
-//     type: process.env.TYPE,
-//     project_id: process.env.PROJECT_ID,
-//     private_key_id: process.env.PRIVATE_KEY_ID,
-//     private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-//     client_email: process.env.CLIENT_EMAIL,
-//     client_id: process.env.CLIENT_ID,
-//     auth_uri: process.env.AUTH_URI,
-//     token_uri: process.env.TOKEN_URI,
-//     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-//     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-//     universe_domain: process.env.UNIVERSE_DOMAIN
-// };
+const config = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN
+};
 
 
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(config)
 });
 
 let db = admin.firestore();
