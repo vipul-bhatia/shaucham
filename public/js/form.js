5664fb65290343d81efc837a3c0a1d483e2ba6a1
Vipul Bhatia <97945914+vipul-bhatia@users.noreply.github.com>
2024-07-01 15:36:02 +0530

    .

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,7 @@
 window.onload = () => {
-    if(sessionStorage.user){
+    if (sessionStorage.user) {
         user = JSON.parse(sessionStorage.user);
-        if(compareToken(user.authToken,user.email)){
+        if (compareToken(user.authToken, user.email)) {
             location.replace('/');
         }
     }
@@ -20,45 +20,45 @@ const notification = document.querySelector('#notification') || null;
 
 
 submitBtn.addEventListener('click', () => {
-    if(name !=null){ //signup page
-        if(name.value.length < 3){
+    if (name != null) { //signup page
+        if (name.value.length < 3) {
             showAlert('name must be 3 letters long');
-        }else if(!email.value.length){
+        } else if (!email.value.length) {
             showAlert('enter your email');
-        } else if(password.value.length < 8){
+        } else if (password.value.length < 8) {
             showAlert('password should be 8 letters long');
-        } else if(!number.value.length){
+        } else if (!number.value.length) {
             showAlert('enter your phone number');
-        } else if(!Number(number.value) || number.value.length < 10){
+        } else if (!Number(number.value) || number.value.length < 10) {
             showAlert('invalid number, please enter valid one');
-        } else if(!tac.checked){
+        } else if (!tac.checked) {
             showAlert('you must agree to our terms and conditions');
-        } else{
+        } else {
             //submit form
             loader.style.display = 'block';
-            sendData('/signup', {
+            sendData('./signup.html', {
                 name: name.value,
                 email: email.value,
                 password: password.value,
                 number: number.value,
                 tac: tac.checked,
                 notification: notification.checked
-                
-        })
+
+            })
         }
-    } 
-    else{
+    }
+    else {
         //login page
-        if(!email.value.length || !password.value.length){
+        if (!email.value.length || !password.value.length) {
             showAlert('fill all the inputs');
         }
-        else{
+        else {
             loader.style.display = 'block';
             sendData('/login', {
                 email: email.value,
                 password: password.value,
 
-        })
+            })
         }
     }
 })
